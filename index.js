53d1e2e5fb697486e028fcb7145e5dbdd02ab191
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-05-05 01:42:20 +0500

    Добавлены файлы проекта

---
+++ index.js
@@ -0,0 +1,29 @@
+const counterOutputLabelNode = document.querySelector('[data-output = "counterOutputLabel"]'); // получил объект надписи для вывода результата
+const counterPlusButtonsNode = document.querySelectorAll('[data-pools]'); // получил коллекцию кнопок добавления бассейнов
+const counterResetButtonNode = document.querySelector('#resetButton');
+const COUNTER_DEFAULT_VALUE = 0;
+// заданы начальные значения для счетчика и надписи
+counterOutputLabelNode.innerHTML = COUNTER_DEFAULT_VALUE; //задали надписи изначальное значение
+let counter = COUNTER_DEFAULT_VALUE;
+
+//к каждому элементу в коллекции привязал функцию addPools, которая будет срабатывать по клику
+counterPlusButtonsNode.forEach(function (Element) {
+	Element.addEventListener('click', addPools);
+});
+//привязал к кнопке функцию обновления счетчинка
+counterResetButtonNode.addEventListener('click', resetCounter)
+// функция для добавления бассейнов
+
+function addPools() {
+	pools_value = parseInt(this.dataset.pools); // достаю значение из объекта, к которому привязан Listener значение атрибута data-pools
+	counter = counter + pools_value;
+	counterOutputLabelNode.innerHTML = counter;
+	if (counterOutputLabelNode.innerHTML > COUNTER_DEFAULT_VALUE) {
+		counterResetButtonNode.classList.remove('hideButton');	
+	} 
+}
+// функция для обновления счетчика
+function resetCounter() {
+	counterOutputLabelNode.innerHTML = COUNTER_DEFAULT_VALUE;
+	counterResetButtonNode.classList.add('hideButton');
+}
