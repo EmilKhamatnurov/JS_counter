cf5ff41e94a494733dd8db432b884fae866224ac
emil-khamatnurov <eh-lens@yandex.ru>
2023-05-05 11:43:30 +0500

    Добавлен прогресс-бар

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const counterOutputLabelNode = document.querySelector('[data-output = "counterOutputLabel"]'); // получил объект надписи для вывода результата
 const counterPlusButtonsNode = document.querySelectorAll('[data-pools]'); // получил коллекцию кнопок добавления бассейнов
 const counterResetButtonNode = document.querySelector('#resetButton');
+const poolProgressBarNode = document.querySelector('#poolProgressBar');
 const COUNTER_DEFAULT_VALUE = 0;
 // заданы начальные значения для счетчика и надписи
-counterOutputLabelNode.innerHTML = COUNTER_DEFAULT_VALUE; //задали надписи изначальное значение
+// counterOutputLabelNode.innerHTML = COUNTER_DEFAULT_VALUE; //задали надписи изначальное значение
 let counter = COUNTER_DEFAULT_VALUE;
 
 //к каждому элементу в коллекции привязал функцию addPools, которая будет срабатывать по клику
@@ -21,10 +22,16 @@ function addPools() {
 	if (counterOutputLabelNode.innerHTML > COUNTER_DEFAULT_VALUE) {
 		counterResetButtonNode.classList.remove('hideButton');	
 	} 
+	if (counter < 100) {
+		poolProgressBarNode.style.width = counter + '%';
+	} else if (counter >= 100) {
+		poolProgressBarNode.style.width = '100%';
+	}
 }
 // функция для обновления счетчика
 function resetCounter() {
 	counter = COUNTER_DEFAULT_VALUE;
 	counterOutputLabelNode.innerHTML = COUNTER_DEFAULT_VALUE;
 	counterResetButtonNode.classList.add('hideButton');
+	poolProgressBarNode.style.width = '0%';
 }
